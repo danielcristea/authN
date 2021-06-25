176f1e89a35c7e1fe376b21a734977a19a5ae0c2
Daniel Cristea <danielcristea87@gmail.com>
2021-06-25 09:56:48 +0300

    base spring security project

---
+++ src/main/java/tech/ing/authN/AuthNApplication.java
@@ -0,0 +1,13 @@
+package tech.ing.authN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthNApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthNApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/ing/authN/AuthNApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.ing.authN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthNApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
