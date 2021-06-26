a61b8c66ddfc12fb0980165e320819d966d52a28
Daniel Cristea <danielcristea87@gmail.com>
2021-06-26 11:48:26 +0300

    login with persisted user details

---
+++ src/main/java/tech/ing/authorization/services/JpaUserDetailsService.java
@@ -0,0 +1,27 @@
+package tech.ing.authorization.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import tech.ing.authorization.model.CustomUserDetails;
+import tech.ing.authorization.model.entities.User;
+import tech.ing.authorization.repositories.UserRepository;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+@Service
+public class JpaUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public CustomUserDetails loadUserByUsername(String username) {
+        Supplier<UsernameNotFoundException> s =
+                () -> new UsernameNotFoundException("Problem during authentication!");
+        User u = userRepository.findUserByUsername(username).orElseThrow(s);
+        return new CustomUserDetails(u);
+    }
+}
